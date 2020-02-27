8bccfde845046474e0ee0c6e2585d6bfeee013e7
Paul Smith <paulrsmithjnr@gmail.com>
2020-02-26 20:56:55 -0500

    Completed up to step 8

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -34,8 +34,10 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
@@ -43,11 +45,16 @@ def login():
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
 
-            # get user id, load into session
-            login_user(user)
+            user = UserProfile.query.filter_by(username=username).first()
 
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            if user is not None and check_password_hash(user.password, password):
+
+                # get user id, load into session
+                login_user(user)
+
+                # remember to flash a message to the user
+                flash('Logged in successfully.', 'success')
+                return redirect(url_for("/secure-page"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
